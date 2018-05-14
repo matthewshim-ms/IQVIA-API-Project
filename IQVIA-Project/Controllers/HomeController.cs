272fee8263470a627fe5aace87b6ebbed7310f74
Matt Shim (TEKsystems) <v-shimma@microsoft.com>
2018-05-14 14:20:26 -0700

    added code to not add/remove duplicate tweets

--- IQVIA-Project/Controllers/HomeController.cs
+++ IQVIA-Project/Controllers/HomeController.cs
@@ -1,15 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using IQVIA_Project.Models;
-//using Microsoft.AspNetCore.Http;
 using System.Net.Http;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
-
 
 namespace IQVIA_Project.Controllers
 {
@@ -17,15 +15,18 @@ namespace IQVIA_Project.Controllers
     {
         public async Task<IActionResult> GetTweets()
         {
-            // Do Later
-            // HttpRequest httpRequest = new HttpRequest();
-
             string original_start_date = "2016-01-01T00:00:00.001Z";
+            DateTime original_start = DateTime.Parse(original_start_date);
 
             DateTime startDate = DateTime.Parse(original_start_date);
             string endDate = "2017-12-31T23%3A59%3A59.001Z";
 
             TweetsViewModel tweets = new TweetsViewModel();
+            Hashtable tweetIDs = new Hashtable();
+
+            // Performance testing between using Hashtable vs. LINQ query
+            // Stopwatch sw = new Stopwatch();
+            // sw.Start();
 
             // Disposes of httpclient when request is completed
             using (HttpClient request = new HttpClient()) {
@@ -34,18 +35,29 @@ namespace IQVIA_Project.Controllers
                 request.DefaultRequestHeaders.Accept.Clear();
 
                 bool lessThan100 = false;
-
-                while(!lessThan100)
+                List<Tweet> listOfTweets = new List<Tweet>();
+                while (!lessThan100)
                 {
-
+                    
                     using (HttpResponseMessage response = await request.GetAsync("?startDate=" + startDate + "&endDate=" + endDate))
                     {
                         if (response.IsSuccessStatusCode)
                         {
                             string tweetList = await response.Content.ReadAsStringAsync();
-                            var listOfTweets = JsonConvert.DeserializeObject<List<Tweet>>(tweetList);
+                            listOfTweets = JsonConvert.DeserializeObject<List<Tweet>>(tweetList);
 
-                            tweets.tweetList.AddRange(listOfTweets);
+
+                            foreach (Tweet twt in listOfTweets)
+                            {
+                                if (!tweetIDs.Contains(twt.id) && startDate > original_start)
+                                {
+                                    tweetIDs.Add(twt.id, 1);
+                                    tweets.tweetList.Add(twt);
+                                }
+                            }
+
+                            // # Method 2 - adding all, then use LINQ
+                            //tweets.tweetList.AddRange(listOfTweets);
 
                             // Get the maximum time stamp in the list of tweets, update the time stamp
                             startDate = listOfTweets.Max(t => t.stamp);
@@ -54,15 +66,29 @@ namespace IQVIA_Project.Controllers
                         }
                         else // bad response
                         {
-                            
+                         
                             lessThan100 = true;
                         }
-            
-                    }        
+                        
+                    }
                 }
-            }
 
-            return View(tweets);
+                //tweets = tweets.tweetList.Where(twt => twt.stamp >= original_start).Select<>ToList();
+
+                //tweets.tweetList = (from tweet in tweets.tweetList
+                //               where tweet.stamp >= original_start
+                //               select tweet).ToList();
+                
+
+                // # Method 2 - with LINQ
+                //listOfTweets = tweets.tweetList.GroupBy(x => x.id, (key, group) => group.First()).ToList();
+                //var nachos = listOfTweets.GroupBy(r => r.id).Select(g => new { Id = g.Key, Count = g.Count() }).Where(c => c.Count > 1).ToList();
+
+                // sw.Stop();
+                
+            }
+            // var time_elapsed = sw.Elapsed;
+            return View(tweets);   
         }
 
 
